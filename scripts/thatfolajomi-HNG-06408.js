f7e4affaea1e1b22a073545085855ca9d2bda17a
thatfolajomi <64877098+thatfolajomi@users.noreply.github.com>
2020-06-03 15:12:46 +0100

    add task2 file to develop branch after fork from original
    Update 1

---
+++ scripts/thatfolajomi-HNG-06408.js
@@ -0,0 +1,5 @@
+var name = "Folajomi Ibrahim";
+var userId = "HNG-06408";
+var email = "thatfolajomi@gmail.com";
+var language = "JavaScript";
+console.log("Hello World" + "," + " " + "this is " + name + " " + "with HNGi7 ID" + " " + userId + " and email " + email + " " + "using " + language + " " + "for stage 2 task" );
